3124462c9085dd53321a9156fffa93df11160a63
SILVANI CUSTODIO DE OLIVEIRA <silvani.oliveira@etec.sp.gov.br>
2021-03-23 18:35:37 -0300

    C# pronto

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace pc_etec1b_projeto_medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double metros = 0.0, cm = 0.0, km = 0.0;
+
+            Console.Write("digite uma medida em metros: ");
+            metros = Convert.ToDouble(Console.ReadLine());
+
+            cm = metros * 100;
+            km = metros / 1000;
+
+            Console.WriteLine("--- Equivalência ---\n"+cm+" cm\n"+metros+" m\n"+km+" KM");
+            Console.ReadKey();
+        }
+    }
+}
